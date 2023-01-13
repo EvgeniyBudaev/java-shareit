4200d96ca416df6caca4e0d4576872bd96587822
Evgeniy Budaev <budaev.e@gmail.com>
2023-01-13 08:18:01 +0300

    sprint 13: removed not used variables

--- src/main/java/ru/practicum/shareit/exception/ExceptionsHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ExceptionsHandler.java
@@ -2,14 +2,11 @@ package ru.practicum.shareit.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
