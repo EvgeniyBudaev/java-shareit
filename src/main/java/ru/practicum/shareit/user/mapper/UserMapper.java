98067992f05e854a369ff3621e878ce8561d730c
Evgeniy Budaev <budaev.e@gmail.com>
2023-01-14 21:07:29 +0300

    sprint 13: fix separator

--- src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -6,6 +6,8 @@ import ru.practicum.shareit.user.model.User;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
     UserDto toUserDto(User user);
+
     User toUser(UserDto userDto);
 }
