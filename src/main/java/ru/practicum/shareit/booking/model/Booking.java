c8515a8ae21bb4c60617d7a9aec1a771eb9521de
Evgeniy Budaev <budaev.e@gmail.com>
2023-01-27 10:49:27 +0300

    test9

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -6,7 +6,17 @@ import lombok.ToString;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Entity
@@ -18,19 +28,24 @@ public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     @ToString.Exclude
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
+
     @ToString.Exclude
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "booker_id")
     private User booker;
+
     @Enumerated(EnumType.STRING)
     @Column(name = "status")
     private Status status;
+
     @Column(name = "start_booking")
     private LocalDateTime start;
+
     @Column(name = "end_booking")
     private LocalDateTime end;
 }
--- src/main/java/ru/practicum/shareit/booking/service/BookingService.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingService.java
@@ -7,13 +7,46 @@ import ru.practicum.shareit.booking.model.State;
 import java.util.List;
 
 public interface BookingService {
+    /**
+     * Реализует бронирование
+     * @param bookerId идентификатор
+     * @param booking Бронь
+     * @return Booking
+     */
     Booking addBooking(long bookerId, Booking booking);
 
+    /**
+     * Реализует подтверждение или отклонение запроса на бронирование
+     * @param ownerId идентификатор Владельца
+     * @param bookingId идентификатор Брони
+     * @param approved статус бронирования
+     * @param accessLevel уровень доступа
+     * @return Booking
+     */
     Booking approveOrRejectBooking(long ownerId, long bookingId, boolean approved, AccessLevel accessLevel);
 
+    /**
+     * Возвращает Бронирование по идентификатору
+     * @param bookingId идентификатор Брони
+     * @param userId идентификатор пользователя
+     * @param accessLevel уровень доступа
+     * @return Booking
+     */
     Booking getBookingById(long bookingId, long userId, AccessLevel accessLevel);
 
+    /**
+     * Возвращает коллекцию Booking для текущего Пользователя
+     * @param state состояние
+     * @param bookerId идентификатор
+     * @return коллекцию Booking
+     */
     List<Booking> getBookingsOfCurrentUser(State state, long bookerId);
 
+    /**
+     * Возвращает коллекцию Booking для текущего Владельца
+     * @param state состояние
+     * @param ownerId идентификатор Владельца
+     * @return коллекцию Booking
+     */
     List<Booking> getBookingsOfOwner(State state, long ownerId);
 }
