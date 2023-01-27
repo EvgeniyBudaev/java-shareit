e38c483248a9de693798a71b6b796efc92f09016
Evgeniy Budaev <budaev.e@gmail.com>
2023-01-27 10:31:49 +0300

    test8

--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -7,13 +7,37 @@ import java.util.List;
 
 public interface UserService {
 
+    /**
+     * Реализует добавление Пользователя в хранилище
+     * @param user объект Пользователь
+     * @return User
+     */
     User addUser(User user) throws DataExistException;
 
+    /**
+     * Реализует обновление полей Пользователя
+     * @param userId идентификатор Пользователя
+     * @param user объект Пользователь с изменениями
+     * @return User
+     */
     User updateUser(long userId, User user) throws DataExistException;
 
+    /**
+     * Возвращает Пользователя по идентификатору
+     * @param userId идентификатор пользователя
+     * @return User
+     */
     User getUserById(long userId);
 
+    /**
+     * Возвращает коллекцию Пользователей
+     * @return коллекцию User
+     */
     List<User> getAllUsers() throws DataExistException;
 
+    /**
+     * Реализует удаление Пользователя из хранилища
+     * @param userId идентификатор Пользователя
+     */
     void removeUser(long userId);
 }
