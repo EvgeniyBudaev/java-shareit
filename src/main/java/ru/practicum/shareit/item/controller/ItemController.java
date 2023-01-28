4cfc1814cf9b4c6475039c5d0f3bf62d1ada292e
Evgeniy Budaev <budaev.e@gmail.com>
2023-01-28 16:51:43 +0300

    item constants UriBuilder

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -20,25 +20,43 @@ import java.util.List;
 @AllArgsConstructor
 public class ItemController {
     private final ItemService itemService;
+    private final String host = "localhost";
+    private final String port = "8080";
+    private final String protocol = "http";
     private final String userIdHeader = "X-Sharer-User-Id";
 
     @PostMapping
     public ResponseEntity<ItemDto> addItem(@RequestHeader(userIdHeader) long userId, @Valid @RequestBody ItemDto itemDto) {
-        UriComponents uriComponents = UriComponentsBuilder.newInstance().scheme("http").host("localhost").port("8080").path("/items").build();
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .scheme(protocol)
+                .host(host)
+                .port(port)
+                .path("/items")
+                .build();
         Logger.logRequest(HttpMethod.POST, uriComponents.toUriString(), itemDto.toString());
         return ResponseEntity.status(201).body(itemService.addItem(userId, itemDto));
     }
 
     @GetMapping("{itemId}")
     public ResponseEntity<ItemDto> getItem(@PathVariable long itemId, @RequestHeader(userIdHeader) long userId) {
-        UriComponents uriComponents = UriComponentsBuilder.newInstance().scheme("http").host("localhost").port("8080").path("/items/{itemId}").build();
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .scheme(protocol)
+                .host(host)
+                .port(port)
+                .path("/items/{itemId}")
+                .build();
         Logger.logRequest(HttpMethod.GET, uriComponents.toUriString(), "пусто");
         return ResponseEntity.ok().body(itemService.getItemById(itemId, userId));
     }
 
     @GetMapping     // Просмотр владельцем списка всех его вещей с указанием названия и описания для каждой
     public ResponseEntity<List<ItemDto>> getAllItems(@RequestHeader(userIdHeader) long userId) {
-        UriComponents uriComponents = UriComponentsBuilder.newInstance().scheme("http").host("localhost").port("8080").path("/items").build();
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .scheme(protocol)
+                .host(host)
+                .port(port)
+                .path("/items")
+                .build();
         Logger.logRequest(HttpMethod.GET, uriComponents.toUriString(), "пусто");
         return ResponseEntity.ok().body(itemService.getAllItems(userId));
     }
@@ -46,9 +64,9 @@ public class ItemController {
     @GetMapping("/search")
     public ResponseEntity<List<ItemDto>> searchItems(@RequestParam String text) {
         UriComponents uriComponents = UriComponentsBuilder.newInstance()
-                .scheme("http")
-                .host("localhost")
-                .port("8080")
+                .scheme(protocol)
+                .host(host)
+                .port(port)
                 .path("/items/")
                 .query("search?text={text}")
                 .build();
@@ -58,7 +76,12 @@ public class ItemController {
 
     @PatchMapping("{itemId}")
     public ResponseEntity<ItemDto> updateItem(@RequestHeader(userIdHeader) long userId, @PathVariable long itemId, @RequestBody ItemDto itemDto) {
-        UriComponents uriComponents = UriComponentsBuilder.newInstance().scheme("http").host("localhost").port("8080").path("/items/{itemId}").build();
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .scheme(protocol)
+                .host(host)
+                .port(port)
+                .path("/items/{itemId}")
+                .build();
         Logger.logRequest(HttpMethod.PATCH, uriComponents.toUriString(), itemDto.toString());
         return ResponseEntity.ok().body(itemService.updateItem(userId, itemId, itemDto));
     }
@@ -66,7 +89,12 @@ public class ItemController {
     @DeleteMapping("{itemId}")
     public ResponseEntity<Void> removeItem(@RequestHeader(userIdHeader) long userId, @PathVariable long itemId) {
         itemService.removeItem(userId, itemId);
-        UriComponents uriComponents = UriComponentsBuilder.newInstance().scheme("http").host("localhost").port("8080").path("/items/{itemId}").build();
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .scheme(protocol)
+                .host(host)
+                .port(port)
+                .path("/items/{itemId}")
+                .build();
         Logger.logRequest(HttpMethod.DELETE, uriComponents.toUriString(), "пусто");
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -74,7 +102,12 @@ public class ItemController {
     @PostMapping("/{itemId}/comment")
     public ResponseEntity<CommentDto> addComment(@RequestHeader(userIdHeader) long userId, @PathVariable long itemId,
                                  @RequestBody @Valid CommentDto commentDto) {
-        UriComponents uriComponents = UriComponentsBuilder.newInstance().scheme("http").host("localhost").port("8080").path("/items/{itemId}/comment").build();
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .scheme(protocol)
+                .host(host)
+                .port(port)
+                .path("/items/{itemId}/comment")
+                .build();
         Logger.logRequest(HttpMethod.POST, uriComponents.toUriString(), commentDto.toString());
         return ResponseEntity.ok().body(itemService.addComment(userId, itemId, commentDto));
     }
