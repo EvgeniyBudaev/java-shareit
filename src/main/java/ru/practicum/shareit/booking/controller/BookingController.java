86de0768ca7f4f8e67255f57c81e8635ad5c16ef
Evgeniy Budaev <budaev.e@gmail.com>
2023-01-28 17:02:36 +0300

    bookings UriBuilder

--- src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 import ru.practicum.shareit.booking.model.AccessLevel;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingInputDto;
@@ -20,32 +22,61 @@ import java.util.List;
 public class BookingController {
     private final BookingService bookingService;
     private final StateEnumConverter converter;
+    private final String host = "localhost";
+    private final String port = "8080";
+    private final String protocol = "http";
     private final String userIdHeader = "X-Sharer-User-Id";
 
     @PostMapping    // Добавление нового запроса на бронирование.
     public ResponseEntity<BookingDto> addBooking(@RequestHeader(userIdHeader) long userId,
                                                  @Valid @RequestBody BookingInputDto bookingInputDto) {
-        Logger.logRequest(HttpMethod.POST, "/bookings", bookingInputDto.toString());
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .scheme(protocol)
+                .host(host)
+                .port(port)
+                .path("/bookings")
+                .build();
+        Logger.logRequest(HttpMethod.POST, uriComponents.toUriString(), bookingInputDto.toString());
         return ResponseEntity.status(201).body(bookingService.addBooking(userId, bookingInputDto));
     }
 
     @PatchMapping("/{bookingId}")   // Подтверждение или отклонение запроса на бронирование.
     public ResponseEntity<BookingDto> approveOrRejectBooking(@PathVariable long bookingId, @RequestParam boolean approved,
                                       @RequestHeader(userIdHeader) long userId) {
-        Logger.logRequest(HttpMethod.PATCH, "/bookings/" + bookingId + "?approved=" + approved, "no body");
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .scheme(protocol)
+                .host(host)
+                .port(port)
+                .path("/bookings/")
+                .query("approved={approved}")
+                .build();
+        Logger.logRequest(HttpMethod.PATCH, uriComponents.toUriString(), "no body");
         return ResponseEntity.ok().body(bookingService.approveOrRejectBooking(userId, bookingId, approved, AccessLevel.OWNER));
     }
 
     @GetMapping("/{bookingId}")   // Получение данных о конкретном бронировании (включая его статус)
     public ResponseEntity<BookingDto> getBookingById(@PathVariable long bookingId, @RequestHeader(userIdHeader) long userId) {
-        Logger.logRequest(HttpMethod.GET, "/bookings/" + bookingId, "no body");
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .scheme(protocol)
+                .host(host)
+                .port(port)
+                .path("/bookings/{bookingId}")
+                .build();
+        Logger.logRequest(HttpMethod.GET, uriComponents.toUriString(), "no body");
         return ResponseEntity.ok().body(bookingService.getBooking(bookingId, userId, AccessLevel.OWNER_AND_BOOKER));
     }
 
     @GetMapping   // Получение списка всех бронирований текущего пользователя (можно делать выборку по статусу).
     public ResponseEntity<List<BookingDto>> getBookingsOfCurrentUser(@RequestParam(defaultValue = "ALL") String state,
                                               @RequestHeader(userIdHeader) long userId) {
-        Logger.logRequest(HttpMethod.GET, "/bookings" + "?state=" + state, "no body");
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .scheme(protocol)
+                .host(host)
+                .port(port)
+                .path("/bookings/")
+                .query("state={state}")
+                .build();
+        Logger.logRequest(HttpMethod.GET, uriComponents.toUriString(), "no body");
         return ResponseEntity.ok().body(bookingService.getBookingsOfCurrentUser(converter.convert(state), userId));
     }
 
@@ -53,7 +84,14 @@ public class BookingController {
     @GetMapping("/owner")
     public ResponseEntity<List<BookingDto>> getBookingsOfOwner(@RequestParam(defaultValue = "ALL") String state,
                                         @RequestHeader(userIdHeader) long userId) {
-        Logger.logRequest(HttpMethod.GET, "/bookings" + "/owner?state=" + state, "no body");
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .scheme(protocol)
+                .host(host)
+                .port(port)
+                .path("/bookings/owner")
+                .query("state={state}")
+                .build();
+        Logger.logRequest(HttpMethod.GET, uriComponents.toUriString(), "no body");
         return ResponseEntity.ok().body(bookingService.getBookingsOfOwner(converter.convert(state), userId));
     }
 }
