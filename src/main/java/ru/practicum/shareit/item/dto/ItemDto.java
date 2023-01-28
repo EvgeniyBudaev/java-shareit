24b1f50be075ab3ac7e7196d039bbf4273708879
Evgeniy Budaev <budaev.e@gmail.com>
2023-01-28 12:44:21 +0300

    updateItem

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -10,10 +10,13 @@ import java.util.List;
 @Data
 public class ItemDto {
     private long id;
+
     @NotBlank(message = "Поле с именем не должно быть пустым.")
     private String name;
+
     @NotBlank(message = "Поле с описанием не должно быть пустым.")
     private String description;
+
     @NotNull(message = "Поле Available не должно быть пустым.")
     private Boolean available;
     private BookingDtoShort lastBooking;
