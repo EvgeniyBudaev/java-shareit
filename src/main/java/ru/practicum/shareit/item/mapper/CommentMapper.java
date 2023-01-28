019be0559abf2ed9e50ccceae4679a39187de564
Evgeniy Budaev <budaev.e@gmail.com>
2023-01-28 15:47:23 +0300

    CommentMapper

--- src/main/java/ru/practicum/shareit/item/mapper/CommentMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/CommentMapper.java
@@ -1,23 +1,12 @@
 package ru.practicum.shareit.item.mapper;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.model.Comment;
 
-@Component
-public class CommentMapper {
-    private final ModelMapper modelMapper;
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+    CommentDto convertToDto(Comment comment);
 
-    public CommentMapper() {
-        modelMapper = new ModelMapper();
-    }
-
-    public CommentDto convertToDto(Comment comment) {
-        return modelMapper.map(comment, CommentDto.class);
-    }
-
-    public Comment convertFromDto(CommentDto commentDto) {
-        return modelMapper.map(commentDto, Comment.class);
-    }
+    Comment convertFromDto(CommentDto commentDto);
 }
