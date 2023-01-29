f70192880bfc8fd0601f371b739671462e156046
Evgeniy Budaev <budaev.e@gmail.com>
2023-01-29 09:00:18 +0300

    Mapping

--- src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.booking.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingDtoShort;
 import ru.practicum.shareit.booking.dto.BookingInputDto;
@@ -10,9 +11,12 @@ import ru.practicum.shareit.user.mapper.UserMapper;
 
 @Mapper(componentModel = "spring", uses = {UserMapper.class, ItemMapper.class})
 public interface BookingMapper {
+
     BookingDto convertToDto(Booking booking);
 
+    @Mapping(target = "bookerId", source = "booker.id")
     BookingDtoShort convertToDtoShort(Booking booking);
 
+    @Mapping(target = "item.id", source = "itemId")
     Booking convertFromDto(BookingInputDto bookingInputDto);
 }
