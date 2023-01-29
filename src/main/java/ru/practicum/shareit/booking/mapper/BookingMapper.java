d0607fb32a3f544e1ab96983b3b8ae92db342800
Evgeniy Budaev <budaev.e@gmail.com>
2023-01-29 08:49:24 +0300

    uses UserMapper and ItemMapper

--- src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
@@ -5,8 +5,10 @@ import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingDtoShort;
 import ru.practicum.shareit.booking.dto.BookingInputDto;
 import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.item.mapper.ItemMapper;
+import ru.practicum.shareit.user.mapper.UserMapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {UserMapper.class, ItemMapper.class})
 public interface BookingMapper {
     BookingDto convertToDto(Booking booking);
 
