7527deea92f6bfb68238dba119257ea04337a8b8
Evgeniy Budaev <budaev.e@gmail.com>
2023-02-26 16:08:37 +0300

    add-docker24

--- gateway/src/main/java/ru/practicum/shareit/common/Header.java
+++ gateway/src/main/java/ru/practicum/shareit/common/Header.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.common;
 
 public class Header {
-    public final static String userIdHeader = "X-Sharer-User-Id";
+    public static final String userIdHeader = "X-Sharer-User-Id";
 
 }
